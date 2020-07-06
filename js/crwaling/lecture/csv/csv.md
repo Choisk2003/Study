c8c2da9c1fe341078f9df815f912d5f3f78d260a
최승교 <choisk1116@gmail.com>
2020-07-06 12:07:55 +0900

    study crwaling

---
+++ js/crwaling/lecture/csv/csv.md
@@ -0,0 +1,8 @@
+#csv(comma separated value)
+
+, 와 줄바꿈으로 구분된 값
+
+```csv
+최승교, 18살
+이지수, 17살
+```
---
+++ js/crwaling/lecture/xlsx/xlsx-parse.js
@@ -0,0 +1,30 @@
+const xlsx = require("xlsx");
+const axios = require("axios"); //ajax 라이브러리
+const cheerio = require("cheerio"); // html 파싱
+
+const workbook = xlsx.readFile("xlsx/data.xlsx");
+
+const ws = workbook.Sheets.영화목록;
+
+const records = xlsx.utils.sheet_to_json(ws);
+
+for (const [i, r] of records.entries()) {
+  console.log(i, r.제목, r.링크);
+}
+
+const crawler = async () => {
+  await Promise.all(
+    records.map(async (r) => {
+      const response = await axios.get(r.링크);
+      if (response.status === 200) {
+        //응답 성공
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const text = $(".score.score_left .star_score").text();
+        console.log(r.제목, "평점", text.trim());
+      }
+    })
+  );
+};
+
+crawler();
